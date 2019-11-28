b2d3e48630acf9ec5d32b8e2879be6887c5fbe9e
Nisar Ahmed <stayingcool@gmail.com>
2019-11-28 17:15:33 +1100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+declare const $: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'angular-datatables';
+
+  ngOnInit(): void {
+  //   $(document).ready(function() {
+  //     $('#example').DataTable();
+  // } );
+
+  $(document).ready(function() {
+    var table = $('#example').DataTable( {
+        lengthChange: false,
+        buttons: [ 'excel', 'colvis' ]
+    } );
+ 
+    table.buttons().container()
+        .appendTo( '#example_wrapper .col-md-6:eq(0)' );
+} );
+  }
 }
